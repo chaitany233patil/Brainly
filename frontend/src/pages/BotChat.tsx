8c74770fa2a3faae945853c358deea962a888368
Chaitany233patil <patilchaitany529@gmail.com>
2025-04-12 23:09:12 +0530

    added some user experience , autoscrolling in chat bot

--- frontend/src/pages/BotChat.tsx
+++ frontend/src/pages/BotChat.tsx
@@ -1,31 +1,51 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import getBotResponse from "../ai";
 
 export function BotChat() {
   const messageRef = useRef<HTMLInputElement>(null);
+  const scrollRef = useRef(null);
+  const [loading, setLoading] = useState(false);
   const [Messages, setMessages] = useState([
     {
       role: "assistant",
-      content: "How can i help you today",
+      content: "Hello! How can I help you today?",
     },
   ]);
+
+  useEffect(() => {
+    console.log("hii");
+    if (scrollRef.current) {
+      scrollRef.current.scrollTo({
+        top: scrollRef.current.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [Messages]);
+
   async function getResponse() {
+    setLoading(true);
     //@ts-ignore
     setMessages((prev) => [
       ...prev,
       { role: "user", content: messageRef.current?.value },
     ]);
     const res = await getBotResponse(messageRef.current?.value as string);
+    (messageRef.current as HTMLInputElement).value = "";
     //@ts-ignore
     setMessages((prev) => [...prev, res]);
+    setLoading(false);
   }
   return (
     <div className=" absolute bottom-20 right-30">
       <div className="bg-[#e0e7ff] h-140 w-160 rounded-3xl p-3 flex flex-col justify-between">
-        <div className="flex flex-col overflow-auto">
-          {Messages.map((message) => {
+        <div
+          className="flex flex-col overflow-auto no-scrollbar"
+          ref={scrollRef}
+        >
+          {Messages.map((message, idx) => {
             return (
               <span
+                key={idx}
                 className={`bg-white p-3 rounded-2xl mb-2 ${
                   message.role == "assistant"
                     ? "self-start mr-30"
@@ -36,13 +56,18 @@ export function BotChat() {
               </span>
             );
           })}
+          {loading && (
+            <span className="bg-white p-3 rounded-2xl mb-2 self-start mr-30">
+              Responding...
+            </span>
+          )}
         </div>
         <div className="flex gap-2">
           <input
             ref={messageRef}
             className="flex-1 border-1 border-gray-200 rounded-2xl bg-amber-50 p-3"
             type="text"
-            placeholder="hello"
+            placeholder="Ask anything"
           />
           <button
             onClick={getResponse}
