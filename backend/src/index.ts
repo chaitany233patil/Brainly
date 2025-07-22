e0f79e9c54311021dd1eb3db16289cba4fafd4e5
Chaitany233patil <patilchaitany529@gmail.com>
2025-07-22 22:54:53 +0530

    added --output dist

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,3 @@
-// src/index.ts
 import "dotenv/config";
 import express from "express";
 import { connectDB } from "./db";
@@ -6,9 +5,9 @@ import cors from "cors";
 import authRouter from "./routes/auth.route";
 import contentRouter from "./routes/content.route";
 import shareBrainRouter from "./routes/BrainShare.route";
-import { VercelRequest, VercelResponse } from "@vercel/node";
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
@@ -19,9 +18,6 @@ app.use("/api/v1", authRouter);
 app.use("/api/v1/content", contentRouter);
 app.use("/api/v1/brain/share", shareBrainRouter);
 
-// ❌ REMOVE app.listen() for Vercel
-// ✅ INSTEAD: export the handler
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  app(req, res);
-};
+app.listen(PORT, () => {
+  console.log(`Server listing on ${PORT}`);
+});
