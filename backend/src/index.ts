e496e97e08ab2dcf3272697e1349f42d75770efd
Chaitany233patil <patilchaitany529@gmail.com>
2025-07-22 22:51:58 +0530

    added vercel node for vercel delpoyment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+// src/index.ts
 import "dotenv/config";
 import express from "express";
 import { connectDB } from "./db";
@@ -5,9 +6,9 @@ import cors from "cors";
 import authRouter from "./routes/auth.route";
 import contentRouter from "./routes/content.route";
 import shareBrainRouter from "./routes/BrainShare.route";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
 const app = express();
-const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
@@ -18,6 +19,9 @@ app.use("/api/v1", authRouter);
 app.use("/api/v1/content", contentRouter);
 app.use("/api/v1/brain/share", shareBrainRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server listing on ${PORT}`);
-});
+// ❌ REMOVE app.listen() for Vercel
+// ✅ INSTEAD: export the handler
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  app(req, res);
+};
