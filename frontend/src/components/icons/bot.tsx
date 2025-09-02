4358702e5690736d1c5b4ba776beeeb1f179bc32
Chaitany233patil <patilchaitany529@gmail.com>
2025-09-02 23:21:17 +0530

    fix warning

--- frontend/src/components/icons/bot.tsx
+++ frontend/src/components/icons/bot.tsx
@@ -14,7 +14,7 @@ export function BotIcon(props: BotIconType) {
         props.onClick();
         setClose((prev) => !prev);
       }}
-      className="bg-gradient-to-r from-[#8956f3] via-purple-600 to-[#9010c6] text-white absolute  bottom-8 right-10 text-white p-3 text-xl font-bold 
+      className="bg-gradient-to-r from-[#8956f3] via-purple-600 to-[#9010c6] text-white absolute  bottom-8 right-10 p-3 text-xl font-bold 
       rounded-tl-4xl rounded-bl-4xl rounded-tr-4xl cursor-pointer inset-shadow-sm inset-shadow-[#7f7ac2]-200"
     >
       {close ? <BotButtonCloseIcon /> : <BotButtonIcon />}
